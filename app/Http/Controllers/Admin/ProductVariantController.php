44894a96a69adf33b24a774d6f4a01a96f87a276
longnt <longnt2k4@gmail.com>
2025-09-12 00:52:23 +0700

    destroy-restore-forcedelete-variant

--- app/Http/Controllers/Admin/ProductVariantController.php
+++ app/Http/Controllers/Admin/ProductVariantController.php
@@ -62,6 +62,38 @@ class ProductVariantController extends Controller
             return back()->with('error', 'Không thể cập nhật sản phẩm' . $th->getMessage());
         }
     }
+    public function destroy(string $id)
+    {
+        try {
+            $productVariant = ProductVariant::findOrFail($id);
+            $productVariant->delete();
+            return redirect()->route('admin.product_variants.index')->with('success', 'Xóa thành công, đã thêm vào thùng rác');
+        } catch (\Throwable $th) {
+            return back()->with('error','Không thể xóa sản phẩm' .$th->getMessage());
+        }
+    }
+    public function trash(){
+        $productVariantDeleted = productVariant::onlyTrashed()->latest('id')->paginate(10);
+        return view('admin.product_variants.trash', compact('productVariantDeleted'));
+    }
+    public function restore(string $id){
+        try {
+            $productVariant = ProductVariant::onlyTrashed()->findOrFail($id);
+            $productVariant->restore();
+            return redirect()->route('admin.product_variants.trash')->with('success','Khôi phục biến thể thành công');
+        } catch (\Throwable $th) {
+            return back()->with('error','Không thể khôi phục sản phẩm' . $th->getMessage());
+        }
+    }
+    public function forceDelete(string $id){
+        try {
+            $productVariant = ProductVariant::onlyTrashed()->findOrFail($id);
+            $productVariant->forceDelete();
+            return redirect()->route('admin.product_variants.trash')->with('success','Xóa cứng biến thể thành công');
+        } catch (\Throwable $th) {
+            return back()->with('error','Không thể xóa sản phẩm' . $th->getMessage());
+        }
+    }
     private function generateBarcode()
     {
         // Sinh barcode ngẫu nhiên 12 số, thêm checksum để thành EAN13
